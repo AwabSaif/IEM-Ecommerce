e745d6b73d8845c6085f842283be5fbe322af57a
Awab Saif <awabtaz9090@gmail.com>
2023-12-19 02:48:37 +0300

    fix some bug and connect to DB

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,32 @@
- const dotenv = require("dotenv").config();
- const express = require("express");
- const mongoose = require("mongoose");
- const bodyParer = require("body-parser");
- const cors = require("cors");
+const dotenv = require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParer = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+//Connrct to DB and start server
+
+mongoose.connect(process.env.MONGODB_URI, {
+   
+  });
+const db = mongoose.connection;
+
+db.on('error',()=>{
+    console.log("Connection Error");
+})
+db.once('open',()=>{
+    console.log("Connected to DB !");
+})
+
+app.listen(PORT,()=>{
+    try {
+        console.log(`IEM Server Running on port ${PORT}...`);
+        
+    } catch (err) {
+        console.log(err);
+    }
+})
